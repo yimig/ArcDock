a789d09810107f23db701c067f33576ae2c49b9f
yimig <zhangyiming@programmer.net>
2023-04-17 16:38:35 +0800

    finish receive function

--- ArcDock/ReceiveSettingWindow.xaml.cs
+++ ArcDock/ReceiveSettingWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using ArcDock.Data.Json;
+﻿using ArcDock.Data;
+using ArcDock.Data.Json;
+using log4net;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,6 +20,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using System.Xml;
 
 namespace ArcDock
 {
@@ -57,6 +64,10 @@ namespace ArcDock
 
         private const int VERSION = 1;
         private const int PORT = 3052;
+        private static ILog log = LogManager.GetLogger("SocketClient");
+        private int packageSize;
+        private string fileName;
+        private const string FILE_PATH = @"template\";
 
         public ReceiveSettingWindow()
         {
@@ -69,72 +80,109 @@ namespace ArcDock
         {
             IsConnected = icon;
             Status = status;
+            log.Info(status);
+        }
+
+        private NetAsync CreateRequestPackage()
+        {
+            return new NetAsync()
+            {
+                Version = VERSION,
+                Code = "200",
+                Message = "",
+                Token = ""
+            };
+        }
+
+        private NetAsync CreateRefusePackage()
+        {
+            return new NetAsync()
+            {
+                Version = VERSION,
+                Code = "505",
+                Message = "客户端拒绝接收文件",
+                Token = ""
+            };
         }
 
         private void StartClient()
         {
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            globalSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SetStatus(1, "客户端初始化完毕，等待连接服务端");
             try
             {
-                var endPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
-                socket.BeginConnect(endPoint, resultAsync =>
-                {
-                    SetStatus(1, "连接成功，等待交换信息");
-                    var text = SocketReceiveText(socket);
-                    SocketSendText(socket, text);
-                    SetStatus(1, "验证完毕，正在接收配置文件");
-                    var res = SocketReceiveText(socket);
-                }, null);
-                SetStatus(1, "配置文件成功接收");
-                globalSocket = socket;
-            }
-            catch (Exception ex)
+                BeginReceive();
+            } catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-                SetStatus(0,ex.Message);
+                log.Error("Socket服务异常：", ex);
             }
+
         }
 
-
-        private string SocketReceiveText(Socket socket)
+        private async void BeginReceive() 
         {
-            string res = "";
-            byte[] buffer = new byte[1024];
-            try
+            var endPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+            await globalSocket.ConnectAsync(endPoint);
+            SetStatus(0, "服务端已连接");
+
+            var requestStr = JsonConvert.SerializeObject(CreateRequestPackage());
+            var sendBytes = Encoding.UTF8.GetBytes(requestStr);
+            await globalSocket.SendAsync(new ArraySegment<byte>(sendBytes), 0);
+
+            var buffer = new ArraySegment<byte>(new byte[1024]);
+            var received = await globalSocket.ReceiveAsync(buffer, SocketFlags.None);
+            var response = Encoding.UTF8.GetString(buffer.ToArray(), 0, received);
+            log.Debug("收到来自" + globalSocket.RemoteEndPoint.ToString() + "的报文：\n" + response);
+            NetAsync receiveObj = JsonConvert.DeserializeObject<NetAsync>(response);
+            if (receiveObj.Code == "200")
             {
-                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, socketReceive =>
+
+                SetStatus(0, "与服务端认证完毕");
+                if(MessageBox.Show("是否接收配置文件："+receiveObj.Message+"?","接受确认",MessageBoxButton.OKCancel) == MessageBoxResult.OK)
                 {
-                    int length = socket.EndReceive(socketReceive);
-                    if (length > 0)
+                    packageSize = receiveObj.Size;
+                    fileName = receiveObj.Message;
+
+                    await globalSocket.SendAsync(buffer, 0);
+                    var fileBuffer = new ArraySegment<byte>(new byte[packageSize]);
+                    var receivedFile = await globalSocket.ReceiveAsync(fileBuffer, SocketFlags.None);
+                    var responseFile = Encoding.UTF8.GetString(fileBuffer.ToArray(), 0, receivedFile);
+                    log.Debug("收到来自" + globalSocket.RemoteEndPoint.ToString() + "的报文：\n" + responseFile);
+                    NetAsync fileObj = JsonConvert.DeserializeObject<NetAsync>(responseFile);
+                    SetStatus(0, "成功接收到配置");
+
+                    if (fileObj.GlobalSetting != null)
                     {
-                        res = Encoding.UTF8.GetString(buffer, 0, length);
+                        if(MessageBox.Show("目标服务同时发送了全局配置，是否覆盖本地全局配置？","替换全局配置",MessageBoxButton.OKCancel)==MessageBoxResult.OK)
+                        {
+                            Properties.Settings.Default.IsEnableRules = fileObj.GlobalSetting.IsEnableCheck;
+                            Properties.Settings.Default.UserPrintApi = fileObj.GlobalSetting.PrintApi;
+                            Properties.Settings.Default.Save();
+                            new PythonEnvironment().UpdateCode(fileObj.GlobalSetting.Code);
+
+                        }
                     }
-                }, null);
+                    SaveConfig(fileObj.Message);
+                }
+                else
+                {
+                    var refuseBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(CreateRefusePackage()));
+                    await globalSocket.SendAsync(new ArraySegment<byte>(refuseBytes), 0);
+                    SetStatus(2, "用户选择了拒绝接收配置");
+                    return;
+                }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                SetStatus(0, ex.Message);
-            }
-            return res;
         }
 
-        private void SocketSendText(Socket socket, string text)
+        private void SaveConfig(string text)
         {
-            byte[] data = new byte[1024];
-            data = Encoding.UTF8.GetBytes(text);
-            try
-            {
-                socket.BeginSend(data, 0, data.Length, SocketFlags.None, socketResult =>
-                {
-                    socket.EndSend(socketResult);
-                }, null);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                SetStatus(0, ex.Message);
-            }
+            TextWriter tWriter = new StreamWriter(new FileStream(FILE_PATH+fileName, FileMode.Create));
+            tWriter.Write(text);
+            tWriter.Close();
+            MessageBox.Show("配置已保存,将重启程序以应用修改。");
+            System.Windows.Forms.Application.Restart();
+            Environment.Exit(0);
         }
 
         private void BtnConnect_Click(object sender, RoutedEventArgs e)
--- ArcDock/SendSettingWindow.xaml.cs
+++ ArcDock/SendSettingWindow.xaml.cs
@@ -20,6 +20,11 @@ using System.Security.Cryptography;
 using Newtonsoft.Json;
 using System.IO;
 using Path = System.IO.Path;
+using log4net;
+using ArcDock.Data;
+using Microsoft.Scripting.Hosting.Shell;
+using System.Runtime.InteropServices;
+using static IronPython.Modules._ast;
 
 namespace ArcDock
 {
@@ -56,13 +61,13 @@ namespace ArcDock
                 }
             }
         }
-
         private string token;
-
         private Config config;
+        private string fileJson;
 
         private const int VERSION = 1;
         private const int PORT = 3052;
+        private static ILog log = LogManager.GetLogger("SocketServer");
 
         public SendSettingWindow(Config config)
         {
@@ -84,35 +89,48 @@ namespace ArcDock
         private string GetTokenJson()
         {
             token = GetToken();
+            fileJson = CreateFileJson();
             var netJson = new NetAsync()
             {
                 Version = VERSION,
                 Code = "200",
                 Message = Path.GetFileName(config.FilePath),
-                Token = token
+                Token = token,
+                Size = Encoding.UTF8.GetByteCount(fileJson)
             };
             return JsonConvert.SerializeObject(netJson);
         }
 
-        //private string GetFileJson()
-        //{
-        //    TextReader tReader = new StreamReader(new FileStream(config.FilePath, FileMode.Open));
-        //    var templateHtml = tReader.ReadToEnd();
-        //    tReader.Close();
-        //    var globalSetting = new GlobalSettingJson()
-        //    {
-        //        IsEnableCheck = Setting
-        //    };
-        //    var resultObj = new NetAsync()
-        //    {
-        //        Version = VERSION,
-        //        Code = "200",
-        //        Message = templateHtml,
-        //        Token = token
-        //    };
-        //}
+        private string CreateFileJson()
+        {
+            TextReader tReader = new StreamReader(new FileStream(config.FilePath, FileMode.Open));
+            var templateHtml = tReader.ReadToEnd();
+            tReader.Close();
+            var resultObj = new NetAsync()
+            {
+                Version = VERSION,
+                Code = "200",
+                Message = templateHtml,
+                Token = token,
+            };
+            if(CbGlobal.IsChecked == true)
+            {
+                resultObj.GlobalSetting = new GlobalSettingJson()
+                {
+                    IsEnableCheck = Properties.Settings.Default.IsEnableRules,
+                    PrintApi = Properties.Settings.Default.UserPrintApi,
+                    Code = PythonEnvironment.GetPythonCode()
+                };
+            }
+            return JsonConvert.SerializeObject(resultObj);
+        }
 
-        private string CheckReceiveJson(string receiveStr)
+        private string GetFileJson()
+        {
+            return fileJson;
+        }
+
+        private string CheckVersionReceive(NetAsync receive)
         {
             NetAsync resultObj = new NetAsync()
             {
@@ -121,46 +139,54 @@ namespace ArcDock
                 Message = "封包错误",
                 Token = ""
             };
-            if (receiveStr != null)
+            if (receive != null)
             {
-                var receiveObj = JsonConvert.DeserializeObject<NetAsync>(receiveStr);
-                if(receiveObj.Code == "200")
+                if(receive.Code == "200")
                 {
-                    if (receiveObj.Token != token) resultObj = new NetAsync()
-                    {
-                        Version = VERSION,
-                        Code = "511",
-                        Message = "鉴权失败，无权访问",
-                        Token = ""
-                    };
-                    else if (receiveObj.Version != VERSION) resultObj = new NetAsync()
+                    if (receive.Version != VERSION) resultObj = new NetAsync()
                     {
                         Version = VERSION,
                         Code = "505",
                         Message = "客户端版本不匹配，请升级客户端",
                         Token = ""
                     };
-                    else {
-                        TextReader tReader = new StreamReader(new FileStream(config.FilePath, FileMode.Open));
-                        var templateHtml = tReader.ReadToEnd();
-                        tReader.Close();
-                        resultObj = new NetAsync()
-                        {
-                            Version = VERSION,
-                            Code = "200",
-                            Message = templateHtml,
-                            Token = token
-                        };
-                    };
+                    else resultObj = new NetAsync() { Code = "200" };
                 }
             }
-            return JsonConvert.SerializeObject(resultObj);
+            return resultObj.Code == "200" ? GetTokenJson() : JsonConvert.SerializeObject(resultObj);
+        }
+
+        private string CheckTokenReceive(NetAsync receive)
+        {
+            NetAsync resultObj = new NetAsync()
+            {
+                Version = VERSION,
+                Code = "500",
+                Message = "封包错误",
+                Token = ""
+            };
+            if (receive != null)
+            {
+                if (receive.Code == "200")
+                {
+                    if (receive.Token != token) resultObj = new NetAsync()
+                    {
+                        Version = VERSION,
+                        Code = "511",
+                        Message = "鉴权失败，无权访问",
+                        Token = ""
+                    };
+                    else resultObj = new NetAsync() { Code = "200" };
+                }
+            }
+            return resultObj.Code == "200" ? GetFileJson() : JsonConvert.SerializeObject(resultObj);
         }
 
         private void SetStatus(int icon, string status)
         {
             IsConnected = icon;
             Status = status;
+            log.Info(status);
         }
 
         private string GetLocalIP()
@@ -177,108 +203,100 @@ namespace ArcDock
 
         private void StartServer()
         {
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
                 IP = GetLocalIP();
-                var endPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
-                socket.Bind(endPoint);
-                socket.Listen(10);
-                SetStatus(1, "初始化完成，正在等待接收方访问");
-                globalSocket = socket;
-                AcceptSocket(socket);
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+                Socket listener = new Socket(
+                    ipEndPoint.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+                globalSocket = listener;
+                listener.Bind(ipEndPoint);
+                BeginListen();
             } catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                log.Error("Socket服务器启动失败",ex);
                 this.Close();
             }
 
         }
 
-        private void AcceptSocket(Socket socket)
+        private async void BeginListen()
         {
-            var Result = socket.BeginAccept(asyncSocket =>
-            {
-                var client = socket.EndAccept(asyncSocket);
-                SetStatus(0, client.RemoteEndPoint.ToString() + "已连接");
-                SocketSendText(client, GetTokenJson());
-                var resStr = SocketReceiveText(client);
-                var reSendStr = CheckReceiveJson(resStr);
-                SocketSendText(client,reSendStr);
-                AcceptSocket(socket);
-            },null);
-        }
-
-        private string SocketReceiveText(Socket socket)
-        {
-            string res = "";
-            byte[] buffer = new byte[1024];
+            globalSocket.Listen(100);
+            SetStatus(0,"Socket服务启动成功，等待其他客户端连接");
             try
             {
-                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, socketReceive =>
+                var handler = await globalSocket.AcceptAsync();
+                SetStatus(0, "Socket服务已连接");
+                // Receive message.
+                for (int i = 0; i < 2; i++)
                 {
-                    int length = socket.EndReceive(socketReceive);
-                    if (length > 0)
+                    var buffer = new ArraySegment<byte>(new byte[1024]);
+                    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                    byte[] a = buffer.Select(x => x).ToArray();
+                    var response = Encoding.UTF8.GetString(buffer.ToArray(), 0, received);
+                    log.Debug("收到来自" + handler.RemoteEndPoint.ToString() + "的报文：\n" + response);
+                    NetAsync receiveObj = JsonConvert.DeserializeObject<NetAsync>(response);
+                    if(i == 0)
                     {
-                        res = Encoding.UTF8.GetString(buffer, 0, length);
+                        var sendStr = CheckVersionReceive(receiveObj);
+                        if (receiveObj.Code == "200")
+                        {
+                            var echoBytes = Encoding.UTF8.GetBytes(sendStr);
+                            var echoBuffer = new ArraySegment<byte>(echoBytes);
+                            await handler.SendAsync(echoBuffer, 0);
+                            SetStatus(0, "与客户端认证完毕");
+                        }
+                        else {
+                            SetStatus(2, "由于对方请求，服务已断开：" + receiveObj.Message + " 识别代码：" + receiveObj.Code);
+                            break;
+                        }
+                    } else if(i==1)
+                    {
+                        var sendStr = CheckTokenReceive(receiveObj);
+                        if (receiveObj.Code == "200")
+                        {
+                            var echoBytes = Encoding.UTF8.GetBytes(sendStr);
+                            var echoBuffer = new ArraySegment<byte>(echoBytes);
+                            await handler.SendAsync(echoBuffer, 0);
+                            SetStatus(0, "配置数据已成功发送，传送服务停止监听");
+                        }
+                        else {
+                            SetStatus(2, "由于对方请求，服务已断开：" + receiveObj.Message + " 识别代码：" + receiveObj.Code);
+                            break; 
+                        }
                     }
-                }, null);
-            } catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                SetStatus(2, ex.Message);
+                    log.Info("第"+(i+1)+"轮传输结束");
+                }
             }
-            return res;
-        }
-
-        private void SocketSendText(Socket socket, string text)
-        {
-            byte[] data = new byte[1024];
-            data = Encoding.UTF8.GetBytes(text);
-            try
+            catch (SocketException ex)
             {
-                socket.BeginSend(data, 0, data.Length, SocketFlags.None, socketResult =>
-                {
-                    socket.EndSend(socketResult);
-                }, null);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                SetStatus(2, ex.Message);
-            }
-        }
-
-        private void SocketSendFile(Socket socket, string file)
-        {
-            try
-            {
-                socket.BeginSendFile(file, socketResult =>
-                {
-                    socket.EndSend(socketResult);
-                },null);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                SetStatus(2, ex.Message);
+                SetStatus(2, "挂载Socket监听服务时发生异常：" + ex.Message);
             }
         }
 
         private void Window_Closing(object sender, CancelEventArgs e)
         {
-            //if(globalSocket != null)
-            //{
-            //    try
-            //    {
-            //        globalSocket.Shutdown(SocketShutdown.Both);
-            //    } catch (Exception) { } 
-            //    finally
-            //    {
-            //        globalSocket.Close();
-            //        //globalSocket.Dispose();
-            //    }
-            //}
+            if (globalSocket != null)
+            {
+                try
+                {
+                    globalSocket.Shutdown(SocketShutdown.Both);
+                }
+                catch (Exception ex)
+                {
+                    log.Error("关闭Socket服务时发生异常：",ex);
+                }
+                finally
+                {
+                    globalSocket.Close();
+                    globalSocket = null;
+                    log.Info("Socket服务已关闭");
+                }
+            }
         }
     }
 }
